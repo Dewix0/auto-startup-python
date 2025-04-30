2f02ec7c925c10690e64d8faa0ac112dd5568ecb
Даниил Батуров <danbat2006@gmail.com>
2025-04-30 20:35:12 +0300

    added status and corrected language in gui

--- app_gui.py
+++ app_gui.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk,Listbox,font
+from tkinter import ttk,Listbox,font,filedialog
 
 
 def finish():
@@ -8,14 +8,18 @@ def finish():
 
  
 def add_file():
-    btn_add["text"] = f"Добавлен файл"
+    file_path = filedialog.askopenfilename(title="Choose file")
+    status_var.set("Added new file,remember to save")
+    label_status["fg"]="green"
     
     
 def delete_file():
-    btn_delete["text"] = f"Файл удален"     
+    status_var.set("File deleted") 
+    label_status["fg"]="red" 
     
 def save_file():
-    btn_save["text"] = f"Конфигурация сохранена"     
+    status_var.set("Configuration saved")
+    label_status["fg"]="green"  
     
 #Запуска приложения
 root = Tk()
@@ -38,6 +42,13 @@ custom_font = font.Font(size=15, weight="bold")
 label = Label(text="Run anything on windows startup",font=custom_font,anchor=N) 
 label.pack()
 
+
+status_var = StringVar()
+status_var.set("Programm is running")
+status_font=font.Font(weight="bold",size=12)
+label_status=Label(textvariable=status_var,font=status_font,fg="blue")
+label_status.pack(anchor=NW)
+
 listbox =Listbox(root, selectmode=SINGLE)
 listbox.pack(fill=BOTH, expand=True, padx=10, pady=10)
 
@@ -54,8 +65,6 @@ btn_save=ttk.Button(button_frame,text="Save file",command=save_file)
 btn_save.pack(side=LEFT,padx=5)
 
 button_frame.pack(side=BOTTOM)
-
-
 root.protocol("WM_DELETE_WINDOW", finish)
 
 
