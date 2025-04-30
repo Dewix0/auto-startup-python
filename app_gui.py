bdf5b563c310f0c309ddf56b43bbe5496423725c
Даниил Батуров <danbat2006@gmail.com>
2025-04-30 15:31:26 +0300

    learning tkinter

--- app_gui.py
+++ app_gui.py
@@ -1,6 +1,19 @@
 from tkinter import *
+from tkinter import ttk
 
 
+def finish():
+    root.destroy()  # ручное закрытие окна и всего приложения
+    print("App closed")
+
+
+clicks = 0
+ 
+def click_button():
+    global clicks
+    clicks += 1
+    # изменяем текст на кнопке
+    btn["text"] = f"Clicks {clicks}" 
 #Запуска приложения
 root = Tk()
 
@@ -13,8 +26,18 @@ center_x=screen_width//2-150
 center_y=screen_height//2 -200
 
 #Тестовый вывод
-root.title("Anystart")    
+root.title("Anystart")
+root.resizable(False, False)    
 root.geometry(f"500x300+{center_x}+{center_y}")    
 label = Label(text="Run app on windows startup") 
 label.pack()
-root.mainloop()
+
+
+btn = ttk.Button()
+
+btn["text"]="Path"
+btn["command"]=click_button
+btn.pack()
+
+
+root.protocol("WM_DELETE_WINDOW", finish)
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
-
+from app_gui import root
 
 STARTUP_PATH = os.path.join(os.getenv('APPDATA'), 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
 print(STARTUP_PATH)
+
+
+#Запуск приложения
+root.mainloop()
