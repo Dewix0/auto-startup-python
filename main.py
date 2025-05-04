3158d8a87b3262c75bfb95c85e02038ddf9f77d6
Даниил Батуров <danbat2006@gmail.com>
2025-05-04 23:13:23 +0300

    deleted useless file

--- main.py
+++
@@ -1,8 +0,0 @@
-import os
-
-STARTUP_PATH = os.path.join(
-    os.getenv("APPDATA"), "Microsoft", "Windows", "Start Menu", "Programs", "Startup"
-)
-CONFIG_FILE = os.path.join(os.getenv("APPDATA"), "pet_config.json")
-print(STARTUP_PATH)
-print(CONFIG_FILE)
